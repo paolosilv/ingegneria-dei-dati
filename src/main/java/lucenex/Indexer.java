41db06476125a38b93385fd68291a999fcdade1a
Paolo Silvestri <paolo.silv1998@gmail.com>
2022-10-19 23:06:03 +0200

    First Commit

---
+++ src/main/java/lucenex/Indexer.java
@@ -0,0 +1,74 @@
+package lucenex;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.miscellaneous.PerFieldAnalyzerWrapper;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.codecs.Codec;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Indexer {
+
+    protected File[] listFiles(String path){
+        File folder = new File(path);
+        if(folder.isDirectory()) {
+            return folder.listFiles();
+        }
+        return null;
+    }
+
+    protected void indexDocs(Directory directory, Codec codec) throws IOException {
+        long timeIndexing1 = System.currentTimeMillis();
+
+        Analyzer defaultAnalyzer = new StandardAnalyzer();
+        CharArraySet stopWords = new CharArraySet(Arrays.asList("in", "dei", "di", "della", "e", "delle"), true);
+        Map<String, Analyzer> perFieldAnalyzers = new HashMap<>();
+        perFieldAnalyzers.put("nome", new StandardAnalyzer());
+        perFieldAnalyzers.put("contenuto", new StandardAnalyzer(stopWords));
+
+        Analyzer analyzer = new PerFieldAnalyzerWrapper(defaultAnalyzer, perFieldAnalyzers);
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        if (codec != null) {
+            config.setCodec(codec);
+        }
+        IndexWriter writer = new IndexWriter(directory, config);
+        writer.deleteAll();
+        File[] fileList = listFiles("D:\\txtfiles");
+        if (fileList != null) {
+            String queryContent = "";
+            for (File file : fileList) {
+                String fileName = file.getName();
+                fileName = fileName.replaceFirst(".txt", "");
+                FileReader fileReader = new FileReader(file);
+                char[] fileContent = new char[4096];
+                int fileSize = fileReader.read(fileContent);
+                for (int i = 0; i < fileSize; i++) {
+                    queryContent = queryContent + fileContent[i];
+                }
+                Document doc = new Document();
+                doc.add(new TextField("nome", fileName, Field.Store.YES));
+                doc.add(new TextField("contenuto", queryContent, Field.Store.YES));
+
+                writer.addDocument(doc);
+
+                queryContent = "";
+            }
+            writer.commit();
+            writer.close();
+            long timeIndexing2 = System.currentTimeMillis();
+            System.out.println("Indexing time in ms: " + (timeIndexing2 - timeIndexing1) + "\n");
+        }
+    }
+}
---
+++ src/main/java/lucenex/Main.java
@@ -0,0 +1,84 @@
+package lucenex;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.*;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.codecs.simpletext.SimpleTextCodec;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        System.out.println("Welcome in the Apache Lucene Exercise!\nPlease insert the field name for the query: ");
+
+        Scanner in = new Scanner(System.in);
+
+        String fieldName = in.nextLine();
+        System.out.println("Field name: " + fieldName);
+
+        System.out.println("\nPlease insert the query content: ");
+
+        String fieldContent = in.nextLine();
+        System.out.println("Field content: " + fieldContent + "\n");
+
+        long timeIndexing1 = System.currentTimeMillis();
+
+        Indexer indexer = new Indexer();
+        Path path = Paths.get("target/idxMain");
+
+        System.out.println("\nRelevant documents: \n");
+
+        /* Two types of queries: QueryParser and PhraseQuery */
+        Query query = null;
+
+        /*Phrase Query defined by ' ' characters to delimit the content of the query */
+        if (fieldContent.startsWith("'")) {
+            fieldContent = fieldContent.substring(1, fieldContent.length() - 1);
+            /*split words of the phrase*/
+            String[] components = fieldContent.split(" ");
+            query = new PhraseQuery(fieldName, components);
+        } else {
+            /* Query Parser is the default query */
+            QueryParser parser = new QueryParser(fieldName, new StandardAnalyzer());
+            query = parser.parse(fieldContent);
+        }
+
+        try (Directory directory = FSDirectory.open(path)) {
+            indexer.indexDocs(directory, new SimpleTextCodec());
+            try (IndexReader reader = DirectoryReader.open(directory)) {
+                IndexSearcher searcher = new IndexSearcher(reader);
+                searcher.setSimilarity(new ClassicSimilarity());
+                runQuery(searcher, query, true);
+            } finally {
+                directory.close();
+            }
+        }
+        long timeIndexing2 = System.currentTimeMillis();
+        System.out.println("Execution time for the entire process (indexing + query) in ms: " +(timeIndexing2-timeIndexing1) + "\n");
+    }
+
+    private static void runQuery(IndexSearcher searcher, Query query, boolean explain) throws IOException {
+        /* Print the first 6 ranked documents */
+        TopDocs hits = searcher.search(query, 6);
+        for (int i = 0; i < hits.scoreDocs.length; i++) {
+            ScoreDoc scoreDoc = hits.scoreDocs[i];
+            Document doc = searcher.doc(scoreDoc.doc);
+            System.out.println("doc "+scoreDoc.doc + ": "+ doc.get("nome") + " (" + scoreDoc.score +")");
+            if (explain) {
+                Explanation explanation = searcher.explain(query, scoreDoc.doc);
+                System.out.println(explanation);
+            }
+        }
+    }
+}
