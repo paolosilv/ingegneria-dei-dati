adc8354aacc6634190d2aa4b6f57b7aadfac3ed4
Paolo Silvestri <paolo.silv1998@gmail.com>
2023-01-24 15:56:32 +0100

    Updated the project with more readable code!

--- src/main/java/lucenex/Main.java
+++ src/main/java/lucenex/Main.java
@@ -20,52 +20,23 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ParseException {
 
-        System.out.println("Welcome in the Apache Lucene Exercise!\nPlease insert the field name for the query: ");
+        /* Indexing */
+        // indexing("target/idxMain");
 
-        Scanner in = new Scanner(System.in);
+        /* Matching Process */
+        executeMatching("target/idxMain");
+    }
 
-        String fieldName = in.nextLine();
-        System.out.println("Field name: " + fieldName);
 
-        System.out.println("\nPlease insert the query content: ");
 
-        String fieldContent = in.nextLine();
-        System.out.println("Field content: " + fieldContent + "\n");
-
-        long timeIndexing1 = System.currentTimeMillis();
-
+    private static void indexing(String pathIndex){
         Indexer indexer = new Indexer();
         Path path = Paths.get("target/idxMain");
-
-        System.out.println("\nRelevant documents: \n");
-
-        /* Two types of queries: QueryParser and PhraseQuery */
-        Query query = null;
-
-        /*Phrase Query defined by ' ' characters to delimit the content of the query */
-        if (fieldContent.startsWith("'")) {
-            fieldContent = fieldContent.substring(1, fieldContent.length() - 1);
-            /*split words of the phrase*/
-            String[] components = fieldContent.split(" ");
-            query = new PhraseQuery(fieldName, components);
-        } else {
-            /* Query Parser is the default query */
-            QueryParser parser = new QueryParser(fieldName, new StandardAnalyzer());
-            query = parser.parse(fieldContent);
-        }
-
         try (Directory directory = FSDirectory.open(path)) {
             indexer.indexDocs(directory, new SimpleTextCodec());
-            try (IndexReader reader = DirectoryReader.open(directory)) {
-                IndexSearcher searcher = new IndexSearcher(reader);
-                searcher.setSimilarity(new ClassicSimilarity());
-                runQuery(searcher, query, true);
-            } finally {
-                directory.close();
-            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        long timeIndexing2 = System.currentTimeMillis();
-        System.out.println("Execution time for the entire process (indexing + query) in ms: " +(timeIndexing2-timeIndexing1) + "\n");
     }
 
     private static void runQuery(IndexSearcher searcher, Query query, boolean explain) throws IOException {
@@ -81,4 +52,56 @@ public class Main {
             }
         }
     }
+
+    private static void executeMatching(String pathIndex) throws ParseException {
+        System.out.println("Welcome in the Apache Lucene Exercise!\nPlease insert the field name (nome or contenuto) for the query: ");
+        Scanner in = new Scanner(System.in);
+
+        String fieldName = in.nextLine();
+        if(!fieldName.equals("nome") && !fieldName.equals("contenuto")){
+            System.out.println("incorrect field, use only : nome or contenuto");
+            return;
+        }
+        System.out.println("Field name: " + fieldName);
+
+        System.out.println("\nPlease insert the query content: ");
+        String fieldContent = in.nextLine();
+        System.out.println("Field content: " + fieldContent + "\n");
+
+        long timeIndexing1 = System.currentTimeMillis();
+
+        System.out.println("\nRelevant documents: \n");
+        /* Two types of queries: QueryParser and PhraseQuery */
+        Query query = null;
+
+        /* Phrase Query defined by ' ' characters to delimit the content of the query */
+        if (fieldContent.startsWith("'")) {
+            fieldContent = fieldContent.substring(1, fieldContent.length() - 1);
+            /* split words of the phrase */
+            String[] components = fieldContent.split(" ");
+            query = new PhraseQuery(fieldName, components);
+        } else {
+            /* Query Parser is the default query */
+            QueryParser parser = new QueryParser(fieldName, new StandardAnalyzer());
+            query = parser.parse(fieldContent);
+        }
+
+        Path path = Paths.get(pathIndex);
+        try (Directory directory = FSDirectory.open(path)) {
+            try (IndexReader reader = DirectoryReader.open(directory)) {
+                IndexSearcher searcher = new IndexSearcher(reader);
+                searcher.setSimilarity(new ClassicSimilarity());
+                runQuery(searcher, query, true);
+            } finally {
+                directory.close();
+            }
+
+            long timeIndexing2 = System.currentTimeMillis();
+            System.out.println("Execution time for the entire process (indexing + query) in ms: " +(timeIndexing2-timeIndexing1) + "\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
